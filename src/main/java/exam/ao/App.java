aa9e9024034bc50aa5d7f86e05b857ba45c6a921
user11 <user11@gmail.com>
2023-12-21 12:16:39 +0530

    'arithematic operations'

---
+++ src/main/java/exam/ao/App.java
@@ -0,0 +1,18 @@
+package exam.ao;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Arithematic Operators" );
+        System.out.println( "Addition");
+        System.out.println( "Subtraction" );
+        System.out.println( "Multiplication" );
+        System.out.println( "Division" );
+        
+    }
+}
---
+++ src/main/java/exam/ao/Mul.java
@@ -0,0 +1,5 @@
+package exam.ao;
+
+public class Mul {
+
+}
