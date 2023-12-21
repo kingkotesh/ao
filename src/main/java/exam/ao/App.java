be9d4a5769f69d106114fe2ec1a52e6abe999bd6
kingkotesh <128729336+kingkotesh@users.noreply.github.com>
2023-12-21 12:54:27 +0530

    Update App.java

--- src/main/java/exam/ao/App.java
+++ src/main/java/exam/ao/App.java
@@ -9,10 +9,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Arithematic Operators" );
-        System.out.println( "Addition"+Add.Add(10,20));
-        System.out.println( "Subtraction"+Sub.Sub(20,10) );
-        System.out.println( "Multiplication"+Mul.Mul(3,2) );
-        System.out.println( "Division"+Div.Div(10,2) );
+        System.out.println( "Addition"+Add.add(10,20));
+        System.out.println( "Subtraction"+Sub.sub(20,10) );
+        System.out.println( "Multiplication"+Mul.mul(3,2) );
+        System.out.println( "Division"+Div.div(10,2) );
         
     }
 }
